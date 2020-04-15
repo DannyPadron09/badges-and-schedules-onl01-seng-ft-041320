4f3efb38a4943084f538b26336eca9e92dc891a1
Danny Padron <danny.padron09@gmail.com>
2020-04-15 19:51:12 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end 
+
+def batch_badge_creator(attendees)
+  badges = []
+  attendees.collect do |name|
+    badges << badge_maker(name)
+  end
+  return badges
+end 
+
+def assign_rooms(attendees)
+  room = 0 
+  attendees.collect do |name|
+    room += 1 
+    "Hello, #{name}! You'll be assigned to room #{room}!"
+  end
+end 
+
+def printer
+  
